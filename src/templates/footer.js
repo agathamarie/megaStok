ac1a2ae5b195a4bdb33922b15ef61e77e5573de6
alyssonbryan <agatha06arantes@gmail.com>
2025-02-11 00:41:22 -0400

    Fiz a página de Participantes, alterei algumas coisas do footer, e arrumei o header da Yasmin conforme o combinado

--- src/templates/footer.js
+++ src/templates/footer.js
@@ -1,23 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {    const link = document.createElement('link');
+document.addEventListener('DOMContentLoaded', () => {  
+    const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = './src/css/templates/footer.css';
     document.head.appendChild(link);
 
     const footer = document.createElement('footer');
     footer.innerHTML = `
-        <div class="footer">
-            <img src="src/images/logofooter.png" alt="logo" class="logofooter">
+        <fotter class="footer">
+            <img src="src/images/logo.png" alt="MegaStok" class="logo">
             <nav>
                 <ul>
                     <li><a href="/sobre">Sobre o Projeto</a></li>
                     <li><p> | </p></li>
-                    <li><a href="Participantes.html">Participantes</a></li>
+                    <li><a href="participantes.html">Participantes</a></li>
                 </ul>
                 <p class="fakeTermos">Termo de uso      Política de Privadidade</p>
                 <p class="fakeProperty">® megaStok</p>
             </nav>
-        </div>
-    `;
+        </fotter> `
+    ;
 
     document.body.appendChild(footer);
 });
