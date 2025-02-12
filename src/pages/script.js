7c84eab03f272cf8202dbcad9978758aa137a568
Lorena.L.P <107314914+Lorena-Pinheiro@users.noreply.github.com>
2025-02-12 11:53:06 -0300

    mexendo na api

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,46 +1,41 @@
+import header from "../templates/header.js";
+import fetchCategories from "./filtro.js";
+
+
 const StoreContainer = document.querySelector("#StoreContainer");
-const StoreCount = 20;
 
 // Aqui ele ira buscar todos os produtos na Fake Store ᓚ₍ ^. .^₎
 
 const fetchProducts = async () => {
-    for (let i = 1; i <= StoreCount; i++) {
-        await getProduct(i);
-    }
-}
+    const allProductsUrl = `https://fakestoreapi.com/products/`;
+    const resp = await fetch(allProductsUrl);
 
-//Requisição, aonde vamos buscar os dados ⊹ 
-
-const getProduct = async (id) => {
-    const url = `https://fakestoreapi.com/products/${id}`;
-    const resp = await fetch(url);
     const data = await resp.json();
-    createproductCard(data);
-}
 
+    data.forEach(product => createProductCard(product));
+};
 
 // Criação dos cards dos produtos !!
 
-const createproductCard = (product) =>{
+const createProductCard = (product) =>{
     const card = document.createElement('div');
     card.classList.add("StoreCard");
 
-    const title = product.title;
-    const price = `R$ ${product.price.toFixed(2)}`
+    const title = product.title || "Produto sem nome";
+    const price = product.price ? `R$ ${product.price.toFixed(2)}` : "Preço Indisponível";
     const image = product.image;
 
-    const productInnerHTML = `
-    <div class="imgContainer">
-        <img src="${image}" alt="${title}" />
-    </div>
-    <div class="info">
-        <h3 class="title">${title}</h3>
-        <p class="price">${price}</p>
-    </div>
+    card.innerHTML = `
+        <div class="imgContainer">
+            <img src="${image}" alt="${title}" />
+        </div>
+        <div class="info">
+            <h3 class="title">${title}</h3>
+            <p class="price">${price}</p>
+        </div>
     `;
 
-    card.innerHTML = productInnerHTML;
     StoreContainer.appendChild(card);
-}
+};
 
 fetchProducts();
