5b3f68ccd7c77c59bd3f520531b728cfa4127451
Ninjjer <henrique.gianottiv@gmail.com>
2025-02-12 10:54:30 -0400

    header nao está mais pronto

--- src/templates/header.js
+++ src/templates/header.js
@@ -1,20 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const link = document.createElement('link');
+
+    /*const link = document.createElement('link');
         link.rel = 'stylesheet';
         link.href = './src/css/templates/header.css';
         document.head.appendChild(link);
+    */
 
+const header = () => {
+    return `
+        <header class="header">
+            <img src="src/images/logo.png" alt="MegaStok" class="logo">
+            <nav class="navIcons">
+                <a href="/favoritos.html"><img class="icon" alt="Favoritos" src="src/images/favorito.png"></a>
+                <a href="/carrinho.html"><img class="icon" alt="Carrinho" src="src/images/carrinho.png"></a>
+                <a href="/perfilUsuario.html"><img class="userImage" alt="Perfil" src="src/images/user.png"></a>
+            </nav>
+        </header>
+    `;
+};
 
-        const header = `
-            <header class="header">
-                <img src="src/images/logo.png" alt="MegaStok" class="logo">
-                <nav class="navIcons">
-                    <a href="/favoritos.html"><img class="icon" alt="Favoritos" src="src/images/favorito.png"></a>
-                    <a href="/carrinho.html"><img class="icon" alt="Carrinho" src="src/images/carrinho.png"></a>
-                    <a href="/perfilUsuario.html"><img class="userImage" alt="Perfil" src="src/images/user.png"></a>
-                </nav>
-            </header>`
-        ;
-
-        document.body.innerHTML += header;
-});
+    //document.body.innerHTML += header; 
+export default header;
